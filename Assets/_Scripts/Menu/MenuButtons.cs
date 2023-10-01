9149f741c3aa6f978cde5e1ce0a8e612f561d31d
Teemu Nokkanen <teemu.nokkanen99@gmail.com>
2023-10-01 10:16:08 +0300

    Title Screen functionality added

---
+++ Assets/_Scripts/Menu/MenuButtons.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuButtons : MonoBehaviour
+{
+    [SerializeField] private GameObject settingsWindow;
+
+   public void PlayGame()
+    {
+        SceneManager.LoadScene("LevelBlockout");
+    }
+
+    public void SettingButton()
+    {
+        settingsWindow.SetActive(true);
+    }
+
+    public void Credits()
+    {
+        SceneManager.LoadScene("Credits");
+    }
+
+    public void ExitGame()
+    {
+        Application.Quit(); 
+    }
+}
