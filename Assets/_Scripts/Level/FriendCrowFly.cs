bc4ce8a635428f7a442200d17675952ba2ffb3be
Teemu Nokkanen <teemu.nokkanen99@gmail.com>
2023-10-04 22:44:18 +0300

    Add: crow fly script

---
+++ Assets/_Scripts/Level/FriendCrowFly.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FriendCrowFly : MonoBehaviour
+{
+
+    private Transform[] crowWaypoints;
+
+    private float crowSpeed;
+    private float crowRotationSpeed;
+
+    private int crowState;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Move towards waypoint
+        var step = crowSpeed * Time.deltaTime;
+        transform.position = Vector3.MoveTowards(transform.position, crowWaypoints[crowState].position, step);
+
+        //Rotate towards waypoint
+        transform.rotation = Quaternion.Slerp(transform.rotation, crowWaypoints[crowState].rotation, crowRotationSpeed * Time.deltaTime);
+
+    }
+}
